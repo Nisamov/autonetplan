d302e17f6495db19d86a47e706d9afa4c8904109
Nisamov <84858098+Nisamov@users.noreply.github.com>
2024-04-22 10:55:10 +0200

    0.1.17-1

--- autonetplan/autonetplan-restored.sh
+++ autonetplan/autonetplan-restored.sh
@@ -56,38 +56,25 @@ elif [[ $1 == "-b" || $1 == "--backup" ]]; then
 elif [[ $1 == "-l" || $1 == "--license" ]]; then
     # Lectura de fichero de licencia
         sudo less "$program_files/LICENSE.txt"
-
 # Continuacion con el programa
-
 elif [[ $1 == "-x" || $1 == "--execute" ]]; then
     # Continuacion de programa
-
-
     if [[ $2 == "-m" || $2 == "--manual" ]]; then
         sudo nano "$network_dir"
     elif [[ $2 == "-a" || $2 == "--automatic" ]]; then
         # Configuracion automatica
-
-
-
         if [[ $3 == "-iface" || $3 == "--interface" ]]; then
                 # Preguntar por interfaz de red a usar
                 read -p "Ingrese la interfaz de red a usar: " iface
         # Continuacion de programa
-
-
             if [[ $3 == "-f" || $3 == "--fluid" ]]; then
                 # Configuracion de red por DHCP
                 echo "Configuración de red seleccionada con conexion por DHCP"
                 ipfigured="no"
-                
-
-
             elif [[ $3 == "-s" || $3 == "--static" ]]; then
                 # Configuracion de red por ip estatica
                 echo "Configuración de red seleccionada con conexion por ip estatica"
                 ipfigured="false"
-
                 # Continuacion de programa
                     if [[ $5 == "-ip" || $5 == "--ipconfigure" ]]; then
                         # Preguntar por ip a almacenar
@@ -98,60 +85,54 @@ elif [[ $1 == "-x" || $1 == "--execute" ]]; then
                             if [[ $7 == "-lnkd" || $7 == "--linkeddoor" ]]; then
                                 # Preguntar por puerta de enlace
                                 read -p "Ingrese una puerta de enlace: " linkeddoor
-
-
-
-
-
-
-
-
-
-
-
-
+                            else
+                                # Mensaje por error de valores
+                                echo -e "[\e[31m#\e[0m] No se ha ingresado una puerta de enlace: '-ntmk'."
+                            fi
+                            # Llamada del programa configuracion completa
+                            aune-networked
+                            # Tras la configuracion, preguntar si guardar cambios
+                            # Llamada a la funcion de aplicacion de cambios en fichero netplan
+                            netplanapply
                         else
                             # Mensaje por error de valores
-                            echo -e "[\e[31m#\e[0m] No se ha ingresado una puerta de enlace: '-ntmk'."
+                            echo -e "[\e[31m#\e[0m] Error de valores ingresados: '-ntmk'."
+                            # Error por ingreso de valores erroneos
+                            exit 1
                         fi
-                        # Llamada del programa configuracion completa
-                        aune-networked
-                        # Tras la configuracion, preguntar si guardar cambios
-                        # Llamada a la funcion de aplicacion de cambios en fichero netplan
-                        netplanapply
                     else
                         # Mensaje por error de valores
-                        echo -e "[\e[31m#\e[0m] Error de valores ingresados: '-ntmk', valor ingresado: '$6'."
+                        echo -e "[\e[31m#\e[0m] Error de valores ingresados: '-ip'."
                         # Error por ingreso de valores erroneos
                         exit 1
                     fi
                 else
                     # Mensaje por error de valores
-                    echo -e "[\e[31m#\e[0m] Error de valores ingresados: '-ip', valor ingresado: '$5'."
+                    echo -e "[\e[31m#\e[0m] Error de valores ingresados: '-iface'."
                     # Error por ingreso de valores erroneos
                     exit 1
                 fi
             else
                 # Mensaje por error de valores
-                echo -e "[\e[31m#\e[0m] Error de valores ingresados: '-iface', valor ingresado: '$4'."
+                echo -e "[\e[31m#\e[0m] Error de valores ingresados: '-f' o '-s'."
                 # Error por ingreso de valores erroneos
                 exit 1
             fi
         else
             # Mensaje por error de valores
-            echo -e "[\e[31m#\e[0m] Error de valores ingresados: '-f' o '-s', valor ingresado: '$3'."
+            echo -e "[\e[31m#\e[0m] Error de valores ingresados: '-iface' o '--interface'."
             # Error por ingreso de valores erroneos
             exit 1
         fi
     else
         # Mensaje por error de valores
-        echo -e "[\e[31m#\e[0m] Error de valores ingresados: '-m' o '-a', valor ingresado: '$2'."
+        echo -e "[\e[31m#\e[0m] Error de valores ingresados: '-m' o '-a'."
         # Error por ingreso de valores erroneos
         exit 1
     fi
 else
     # Mensaje por error de valores
-    echo -e "[\e[31m#\e[0m] Error de valores ingresados: '-h', '-r', '-b', '-l' o '-x', valor ingresado: '$1'."
+    echo -e "[\e[31m#\e[0m] Error de valores ingresados: '-h', '-r', '-b', '-l' o '-x'."
     # Error por ingreso de valores erroneos
     exit 1
 fi
