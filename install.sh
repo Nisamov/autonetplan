6bdf3b09b45f0ac1f0448b087c65f2a0e6643961
Nisamov <84858098+Nisamov@users.noreply.github.com>
2024-04-29 14:47:51 +0200

    0.3.10.10

--- install.sh
+++ install.sh
@@ -54,15 +54,15 @@ done
 
 # Problemas copia de script - revisar
 # Copiar el script principal al directorio de instalación renombrando el programa como autonetplan
-while [[ ! -f "$INSTALL_DIR/autonetplan/autonetplan" ]]; do
+while [[ ! -f "$INSTALL_DIR/autonetplan" ]]; do
     # Copiar el archivo
-    sudo cp "$SCRIPT_DIR/autonetplan/autonetplan.sh" "$INSTALL_DIR/autonetplan/autonetplan"
+    sudo cp "$SCRIPT_DIR/autonetplan/autonetplan.sh" "$INSTALL_DIR/autonetplan"
     # Verificar si la copia se realizó correctamente
-    if [[ -f "$INSTALL_DIR/autonetplan/autonetplan" ]]; then
+    if [[ -f "$INSTALL_DIR/autonetplan" ]]; then
         # Mensaje de copia exitosa
-        echo "[#] El script principal se ha copiado exitosamente a $INSTALL_DIR/autonetplan/autonetplan"
+        echo "[#] El script principal se ha copiado exitosamente a $INSTALL_DIR/autonetplan"
         # Dar permisos de ejecución al script principal
-        sudo chmod +x "$INSTALL_DIR/autonetplan/autonetplan"
+        sudo chmod +x "$INSTALL_DIR/autonetplan"
         # Mensaje tras otorgar correctamente los permisos
         echo "[#] Permisos necesarios otorgados correctamente"
     else
