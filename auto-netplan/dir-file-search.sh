ceb5c0d9b67876ab66e05f0eef6a058775a26209
Nisamov <84858098+Nisamov@users.noreply.github.com>
2024-05-01 01:48:04 +0200

    0.4.4-15

--- auto-netplan/dir-file-search.sh
+++ auto-netplan/dir-file-search.sh
@@ -4,21 +4,19 @@
 # Variables del programa
 program_config=/etc/autonetplan/autonetplan.conf
 
-# Variables de mensajeria
-## Variables de color
-red_msg="[\e[31m#\e[0m]"
-yellow_msg="[\e[33m#\e[0m]"
-green_msg="[\e[32m#\e[0m]"
-white_msg="[#]"
 ## Variables de mensajes
 msg_revision="[Revision de datos]:"
 
+#red_msg="[\e[31m#\e[0m]"
+#yellow_msg="[\e[33m#\e[0m]"
+#green_msg="[\e[32m#\e[0m]"
+
 # Iniciacion del programa
-echo "$green_msg $msg_revision Revision de integridad de datos del programa autonetplan en curso."
+echo -e "[\e[32m#\e[0m] $msg_revision Revision de integridad de datos del programa autonetplan en curso."
 # Revisar si el fichero de configuracion existe
 if [[ -f $program_config ]]; then
     # Enviar mensaje de existencia de fichero de configuracion
-    echo "$white_msg $msg_revision En ejecucion"
+    echo "[#] $msg_revision En ejecucion"
     # Revisar si la opcion principal esta habilitada
     opcion_aes=$(grep "^autonetplan-enable-search" "$program_config" | cut -d "=" -f2)
     # Comprobar si la opcion esta establecida en true o false
@@ -33,12 +31,12 @@ if [[ -f $program_config ]]; then
 
         elif [[ "$opcion_afe" == "false" ]]; then
             # Aviso de configuracion deshabilitada
-            echo -e "$red_msg $msg_revision La configuracion autonetplan-file-existence esta desactivada."
+            echo -e "[\e[31m#\e[0m] $msg_revision La configuracion autonetplan-file-existence esta desactivada."
         else
-            echo -e "$red_msg $msg_revision No se ha encontrado la linea autonetplan-file-existence."
+            echo -e "[\e[31m#\e[0m] $msg_revision No se ha encontrado la linea autonetplan-file-existence."
         fi
     elif [[ "$opcion_aes" == "false" ]]; then
         # Aviso de configuracion deshabilitada
-        echo -e "$red_msg $msg_revision La configuracion autonetplan-enable-search esta desactivada."
+        echo -e "[\e[31m#\e[0m] $msg_revision La configuracion autonetplan-enable-search esta desactivada."
     fi
 fi
