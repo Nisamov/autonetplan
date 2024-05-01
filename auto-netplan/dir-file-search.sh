9690a2e4c9fc4b0272ee7d3afd98380756c496f7
Nisamov <84858098+Nisamov@users.noreply.github.com>
2024-05-01 13:33:17 +0200

    0.4.4-17-2

--- auto-netplan/dir-file-search.sh
+++ auto-netplan/dir-file-search.sh
@@ -22,6 +22,9 @@ if [[ -f $program_config ]]; then
     # Comprobar si la opcion esta establecida en true o false
     if [[ "$opcion_aes" == "true" ]]; then
         # Revisar si alguna de las otras dos opciones esta activa
+        # Revisa en el siguiente orden: ficheros -> directorios -> rutas propias
+
+        # Opcion 1 - Ficheros
         opcion_afe=$(grep "^autonetplan-file-existence" "$program_config" | cut -d "=" -f2)
         # Comprobar si la opcion esta establecida en true o false
         if [[ "$opcion_afe" == "true" ]]; then
@@ -34,6 +37,36 @@ if [[ -f $program_config ]]; then
             echo -e "[\e[31m#\e[0m] $msg_revision La configuracion autonetplan-file-existence esta desactivada."
         else
             echo -e "[\e[31m#\e[0m] $msg_revision No se ha encontrado la linea autonetplan-file-existence."
+        fi
+
+        # Opcion 2 - Directorios
+        opcion_ade=$(grep "^autonetplan-directory-existence" "$program_config" | cut -d "=" -f2)
+        # Comprobar si la opcion esta establecida en true o false
+        if [[ "$opcion_ade" == "true" ]]; then
+            # Revisa los directorios y su integridad
+
+            # Codigo
+
+        elif [[ "$opcion_ade" == "false" ]]; then
+            # Aviso de configuracion deshabilitada
+            echo -e "[\e[31m#\e[0m] $msg_revision La configuracion autonetplan-directory-existence esta desactivada."
+        else
+            echo -e "[\e[31m#\e[0m] $msg_revision No se ha encontrado la linea autonetplan-directory-existence."
+        fi
+
+        # Opcion 3 - Rutas Manuales (Own-General-Existence)
+        opcion_oge=$(grep "^own-general-existence" "$program_config" | cut -d "=" -f2)
+        # Comprobar si la opcion esta establecida en true o false
+        if [[ "$opcion_oge" == "true" ]]; then
+            # Revisa las rutas indicadas
+
+            # Codigo
+
+        elif [[ "$opcion_oge" == "false" ]]; then
+            # Aviso de configuracion deshabilitada
+            echo -e "[\e[31m#\e[0m] $msg_revision La configuracion own-general-existence esta desactivada."
+        else
+            echo -e "[\e[31m#\e[0m] $msg_revision No se ha encontrado la linea own-general-existence."
         fi
     elif [[ "$opcion_aes" == "false" ]]; then
         # Aviso de configuracion deshabilitada
