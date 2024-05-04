627f40b651f383012d74b5e11a4cf1519c7b3340
Nisamov <84858098+Nisamov@users.noreply.github.com>
2024-05-04 14:33:25 +0200

    0.5.1-2

--- auto-netplan/autonetplan.sh
+++ auto-netplan/autonetplan.sh
@@ -375,15 +375,18 @@ elif [[ $1 == "-x" || $1 == "--execute" ]]; then
                     # Aplicar red
                     netplanapply
                 fi
-                    if [[ $5 != "-ntcd" && $6 == "-ntcd" || $5 != "-ntcd" && $6 == "--networkcard" ]]; then
-                        # Agregar mas configuracion para otras tarjetas de red
-                        # Preguntar por otra tarjeta de red
-                        read -p "¿Deseas configurar una nueva tarjeta de red? [s/n]: " addnwntcd
-                        # Llamar a funcion new-network-card
-                        new-network-card
-                        # Prueba de manejo de valores
-                        echo "[#] SUPUESTAMENTE LA FUNCION 'new-network-card' SE HA EJECUTADO"
-                    fi
+                if [[ $5 != "-ntcd" && $6 == "-ntcd" || $5 != "-ntcd" && $6 == "--networkcard" ]]; then
+                    # Agregar mas configuracion para otras tarjetas de red
+                    # Preguntar por otra tarjeta de red
+                    read -p "¿Deseas configurar una nueva tarjeta de red? [s/n]: " addnwntcd
+                    # Llamar a funcion new-network-card
+                    new-network-card
+                    # Prueba de manejo de valores
+                    echo "[#] SUPUESTAMENTE LA FUNCION 'new-network-card' SE HA EJECUTADO"
+                else
+                    # No ingresa valores en $5
+                    echo "[#] No se ha proporcionado informacion en $5 (valor 5)"
+                fi
                 # Preguntar si mostrar configuracion de red
                 # Llamar a la funcion show_net_configuration
                 show_net_configuration
