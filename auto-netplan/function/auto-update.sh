96391f2a2bb51fb991bfb449d845d07ddf9ea255
Nisamov <84858098+Nisamov@users.noreply.github.com>
2024-05-22 11:29:19 +0200

    0.6.5-6-4

--- auto-netplan/function/auto-update.sh
+++ auto-netplan/function/auto-update.sh
@@ -25,14 +25,31 @@ function aune-autoupdate(){
         # Obtener la última versión desde GitHub sobre el programa
         response=$(curl -s https://api.github.com/repos/Nisamov/autonetplan/releases/latest)
         latest_tag=$(echo "$response" | jq -r .tag_name)
-        echo "Ultimo tag: $latest_tag" # Línea de depuración para mostrar el nombre del tag
+        if [[ $language == "ESP" ]]; then
+            echo "[#] Ultimo tag: $latest_tag" # Línea de depuración para mostrar el nombre del tag
+        elif [[ $language == "ENG" ]]; then
+            echo "[#] Lastest tag: $latest_tag"
+        else
+            echo "[#] Lastest tag: $latest_tag"
+        fi
         # Extraer el número de versión del tag name
         latest_version=$(echo "$latest_tag" | sed -n 's/v\?\([0-9]\+\.[0-9]\+\.[0-9]\+\).*/\1/p')
-        echo "Ultima version: $latest_version" # Línea de depuración para mostrar el número de versión extraído
-
+        if [[ $language == "ESP" ]]; then
+            echo "[#] Ultima version: $latest_version" # Línea de depuración para mostrar el número de versión extraído
+        elif [[ $language == "ENG" ]]; then
+            echo "[#] Lastest version: $latest_version"
+        else
+            echo "[#] Lastest version: $latest_version"
+        fi
         # Verificar si el número de versión se ha extraído correctamente
         if [[ -z "$latest_version" ]]; then
-            echo "[#] Error: No se pudo extraer la última versión desde GitHub."
+            if [[ $language == "ESP" ]]; then
+                echo -e "[\e[31m#\e[0m] Error: No se pudo extraer la última versión desde GitHub."
+            elif [[ $language == "ENG" ]]; then
+                echo -e "[\e[31m#\e[0m] Error: Could not extract the latest version from GitHub."
+            else
+                echo -e "[\e[31m#\e[0m] Error: Could not extract the latest version from GitHub."
+            fi
             exit 1
         fi
 
