cbce21001b89019c1624890f50111397fc645cc9
Nisamov <84858098+Nisamov@users.noreply.github.com>
2024-05-25 11:18:58 +0200

    0.6.8

--- .github/ISSUE_TEMPLATE/reporte-de-errores-en-el-programa-instalación-ejecución.md
+++ .github/ISSUE_TEMPLATE/reporte-de-errores-en-el-programa-instalación-ejecución.md
@@ -21,3 +21,22 @@ Si es posible, adjuntar una captura de pantalla (únicamente mostrando el error)
 
 **Sistema operativo usado durante la ejecución del programa**
 Sistema operativo: '...'.
+
+<!--
+Ejemplo de escritura:
+
+**Error de ejecución**
+Durante la ejecución del comando "autonetplan -x -a -iface -f", el software muestra error y no aplica cambios.
+
+**Modelo de reporte**
+Ubicación del error:
+1. Ir a la/s linea/s '275' del fichero/s '/usr/local/sbin/autonetplan'.
+2. Explicación: 'Parece haber varias lineas mal colocadas.'
+3. Proceso para llegar al error en la ejecución del programa: '...'.
+
+**Capturas de pantalla**
+(Supuestamente se inserta una imagen)
+
+**Sistema operativo usado durante la ejecución del programa**
+Sistema operativo: 'Ubuntu 22.04 LTS'.
+-->
--- auto-netplan/auneupdate.sh
+++ auto-netplan/auneupdate.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Ruta de ficheros del programa
+program_files="/usr/local/sbin/auto-netplan"
 # Declaracion variables
 temp_cloned="/usr/local/sbin/auto-netplan/temp/"
 # Idioma del programa
@@ -8,12 +10,25 @@ language=$(cat $program_files/program-files/language.lg)
 # Instalar git
 sudo apt install git
 # Crear deposito temporal
-while [[ ! -d $temp_cloned ]]; do
-    sudo mkdir $temp_cloned
+while [[ ! -d "$temp_cloned" ]]; do
+    sudo mkdir "$temp_cloned"
     if [[ -d $temp_cloned ]]; then
         # Descargar version mas reciente
-        git clone "https://github.com/Nisamov/autonetplan" $temp_cloned
+        git clone "https://github.com/Nisamov/autonetplan" "$temp_cloned"
         # Eliminar y distribuir ficheros
+        # Preguntar si guardar datos de la configuracion en /etc/autonetplan/autonetplan.conf
+        if [[ $language == "ESP" ]]; then
+            read -p "[?] Desea mantener la configuracion del software? [s/n]: " mantainconf
+        elif [[ $language == "ENG" ]]; then
+            read -p "[?] Do you want to keep the software configuration? [y/n]: " mantainconf
+        else
+            read -p "[?] Do you want to keep the software configuration? [y/n]: " mantainconf
+        fi
+
+        if [[ $mantainconf == "s" || $mantainconf == "y" ]]; then
+            # Crear fichero donde almacenar la informacion de configuracion temporalmente
+            sudo touch "config-data-temp.info" "$temp_cloned"
+        fi
 
         # Fin
     else
