73c6ea8646214a917cbd0927a574c40fb25b3a73
Nisamov <84858098+Nisamov@users.noreply.github.com>
2024-06-06 23:12:24 +0200

    0.8.6-1

--- auto-netplan/function/schedule.sh
+++ auto-netplan/function/schedule.sh
@@ -20,18 +20,84 @@
 program_files="/usr/local/sbin/auto-netplan"
 # Idioma del programa
 language=$(cat "$program_files/program-files/language.lg")
+# Ruta cron
 cronrute="/tmp/crontab.fJIfht/crontab"
+# Fichero autonetplan del directorio autoneconf renombrado como autonetplan
+program_config="/etc/autonetplan/autonetplan.conf"
+# Configuracion de schedule - tiempo configuracion de tareas automaticas
+opcionacdt=$(grep "^autonetplan-cron-default-time" "$program_config" | cut -d "=" -f2)
+
+function changecrontime(){
+    # Cambiar configuracion de cron
+    if [[ $language == "ESP" ]]; then
+        read -p "[?] Deseas cambiar la configuracion y los periodos de tiempo? [s/n]: " confmodify
+    else
+        read -p "[?] Do you want to change the settings and time periods? [y/n]: " confmodify
+    fi
+
+    if [[ $confmodify == "s" || $confmodify == "y" ]]; then
+        if [[ $language == "ESP" ]]; then
+            echo "[#] Ejemplo ingreso de parametros: 0 7 * * * (Ejecutar el script todos los días a las 7:00 AM)"
+            read -p "[#] Ingrese los parametros a ingresar, separados por 1 espacio: " confsetup
+        else
+            echo "[#] Example of parameter input: 0 7 * * * * (Execute the script every day at 7:00 AM)"
+            read -p "[#] Enter the parameters to be entered, separated by 1 space: " confsetup
+        fi
+
+        if [[ $language == "ESP" ]]; then
+            read -p "[?] Esta es la configuracion que has ingresado '$confsetup', deseas proceder? [s/n]: " conformed
+        else
+            read -p "[?] his is the configuration you have entered '$confsetup', do you want to proceed? [y/n]: " conformed
+        fi
+
+        while [[ $conformed != "s" || $conformed != "y" ]]; do
+            if [[ $language == "ESP" ]]; then
+                echo "[#] Ejemplo ingreso de parametros: 0 7 * * * (Ejecutar el script todos los días a las 7:00 AM)"
+                read -p "[#] Ingrese los parametros a ingresar, separados por 1 espacio: " confsetup
+            else
+                echo "[#] Example of parameter input: 0 7 * * * * (Execute the script every day at 7:00 AM)"
+                read -p "[#] Enter the parameters to be entered, separated by 1 space: " confsetup
+            fi
+
+            if [[ $language == "ESP" ]]; then
+                read -p "[?] Esta es la configuracion que has ingresado '$confsetup', deseas proceder? [s/n]: " conformed
+            else
+                read -p "[?] his is the configuration you have entered '$confsetup', do you want to proceed? [y/n]: " conformed
+            fi
+        done
+            if [[ $language == "ESP" ]]; then
+                echo "[#] Configurando fichero de configuracion..."
+            else
+                echo "[#] Configuring configuration file..."
+            fi
+            # Cambiar contenido de $opcionacdt por el definido previamente
+
+    # Si no se desea cambiar la configuracion establecido en el .conf
+    else
+        if [[ $language == "ESP" ]]; then
+            echo "[#] Se ha dejado la configuracion por defecto."
+        else
+            echo "[#] The default configuration has been left."
+        fi
+    fi
+}
+
 
 if [[ $2 == "backup" ]]; then
     if [[ $language == "ESP" ]]; then
         echo "[#] Se ha seleccionado 'backup' como actividad programada."
         # Leer fichero de configuracion para saber cada cuanto tiene que realizar estas actividades
+        echo "[#] El tiempo por defecto establecido para cron es: $opcionacdt."
     else
         echo "[#] 'Backup' mode selected."
     fi
 elif [[ $2 == "update" ]]; then
+    if [[ $language == "ESP" ]]; then
+        echo "[#] Se ha seleccionado 'update' como actividad programada."
+        echo "[#] El tiempo por defecto establecido para cron es: $opcionacdt."
+    else
 
-
+    fi
 fi
 
 
