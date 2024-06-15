dbd019b3657fe912499d729da7438721dc20f7ee
Nisamov <84858098+Nisamov@users.noreply.github.com>
2024-06-16 00:23:00 +0200

    0.9.1-7

--- auto-netplan/function/get.sh
+++ auto-netplan/function/get.sh
@@ -4,6 +4,9 @@
 program_files="/usr/local/sbin/auto-netplan"
 # Idioma del programa
 language=$(cat "$program_files/program-files/language.lg")
+response=$(curl -s https://api.github.com/repos/Nisamov/autonetplan/releases/latest)
+latest_tag=$(echo "$response" | jq -r .tag_name)
+latest_version=$(echo "$latest_tag" | sed -n 's/v\?\([0-9]\+\.[0-9]\+\.[0-9]\+\).*/\1/p')
 
 if [[ "$1" == "-v" || "$1" == "--version" ]]; then
     if [[ $language == "ESP" ]]; then
@@ -13,16 +16,17 @@ if [[ "$1" == "-v" || "$1" == "--version" ]]; then
     fi
 elif [[ "$1" == "-l" || "$1" == "--lastest" ]]; then
 
-    response=$(curl -s https://api.github.com/repos/Nisamov/autonetplan/releases/latest)
-    latest_tag=$(echo "$response" | jq -r .tag_name)
-    latest_version=$(echo "$latest_tag" | sed -n 's/v\?\([0-9]\+\.[0-9]\+\.[0-9]\+\).*/\1/p')
     # Clonar repo
     git clone
     # Preguntar donde guardar version con un read -p
 
     if [[ $language == "ESP" ]]; then
     # Ultima version oficial
- 
+    # Clonar lastest
+    # La ruta será una nueva, creada proximamente
+    sudo git clone -b "$latest_version" --single-branch "https://github.com/Nisamov/autonetplan"
+    # Supuesto resultado
+    # sudo git clone -b "0.9.0" --single-branch "https://github.com/Nisamov/autonetplan"
     else
         echo "test"
     fi
