ef7d0e7c3a27edbb2e7b51c6190df2c604da3716
Nisamov <84858098+Nisamov@users.noreply.github.com>
2024-06-17 08:13:00 +0200

    0.9.1-9

--- auto-netplan/function/get.sh
+++ auto-netplan/function/get.sh
@@ -4,11 +4,15 @@
 program_files="/usr/local/sbin/auto-netplan"
 # Idioma del programa
 language=$(cat "$program_files/program-files/language.lg")
+# Ultima version
 response=$(curl -s https://api.github.com/repos/Nisamov/autonetplan/releases/latest)
+# Lastest tag
 latest_tag=$(echo "$response" | jq -r .tag_name)
+# Extraccion
 latest_version=$(echo "$latest_tag" | sed -n 's/v\?\([0-9]\+\.[0-9]\+\.[0-9]\+\).*/\1/p')
-
-read -p "Donde descargar version (prueba): " descargarutaversion
+# Ruta de descarga ( Se ha seleccionado esta para tras la descarga, desinstalar el previo e instalar esa otra version, por lo tanto, no afectaria al programa recien descargado )
+# Ejemplo de como deberia quedar: /home/autonetplan/autonetplan_0.9.0 ( solo en descargas de ultima version )
+downloadrute_lookback="/home/$USER/autonetplan_$latest_version"
 
 if [[ "$1" == "-v" || "$1" == "--version" ]]; then
     if [[ $language == "ESP" ]]; then
