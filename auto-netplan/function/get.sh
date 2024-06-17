11bd726a4ef04e3170b7d6820b06e42653540cd5
Nisamov <84858098+Nisamov@users.noreply.github.com>
2024-06-17 08:37:02 +0200

    0.9.2

--- auto-netplan/function/get.sh
+++ auto-netplan/function/get.sh
@@ -16,10 +16,34 @@ downloadrute_lookback="/home/$USER/autonetplan_$latest_version"
 
 if [[ "$1" == "-v" || "$1" == "--version" ]]; then
     # Crear un menu de seleccion dodne el usuario pueda ver y seleccionar (no interactuable, pero si de ingresar texto)
-    echo "En pruebas"
+    # Menu manual (no cuento con los conocimientos para hacer algo mas avanzado que esto, lo siento)
+    echo "| Version Selection : "
+    echo "9 | v.0.9.0"
+    echo "8 | v.0.8.0"
+    echo "7 | v.0.7.0"
+    echo "6 | v.0.6.0"
+    echo "5 | v.0.5.0"
+    echo "4 | v.0.4.0"
+    echo "3 | v.0.3.0"
+    echo "2 | v.0.2.0"
+    echo "1 | v.0.1.0"
+    if [[ $language == "ESP" ]]; then
+        read -p "[#] Ingrese la version a descargar (por caracter numerico): " dwnldversion
+    else
+        read -p "[#] Enter the version to download (numeric characters): " dwnldversion
+    fi
+    downloadrute_lookback_spfv="/home/$USER/autonetplan_$dwnldversion"
+    mkdir "$downloadrute_lookback_spfv"
+    
+    
 elif [[ "$1" == "-l" || "$1" == "--lastest" ]]; then
     # Crear ruta
     mkdir -r "$downloadrute_lookback"
     # Ultima version oficial
     git clone -b "$latest_version" --single-branch "https://github.com/Nisamov/autonetplan" "$downloadrute_lookback"
+    if [[ $language == "ESP" ]]; then
+        echo "[#] El software ha sido descargado en $downloadrute_lookback."
+    else
+        echo "[#] Software installed in $downloadrute_lookback."
+    fi
 fi
