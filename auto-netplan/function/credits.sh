01174012b949c45133a3c4a925fbad0c0c5f075d
Nisamov <84858098+Nisamov@users.noreply.github.com>
2024-08-04 23:30:51 +0200

    0.9.4-2

--- auto-netplan/function/credits.sh
+++ auto-netplan/function/credits.sh
@@ -2,43 +2,26 @@
 
 # Creditos a los desarrolladores y a los software usados durante la creacion y desarrollo de este software
 
-# Ejemplo prueba
-
-# Colores
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-BLUE='\033[0;34m'
-CYAN='\033[0;36m'
-WHITE='\033[0;37m'
+# Variables
 YELLOW='\033[1;33m'
-NC='\033[0m' # Sin color
 
 # Título
-echo -e "${CYAN}********************************************${NC}"
-echo -e "${CYAN}* ${YELLOW}Créditos del Desarrollo del Software${CYAN} *${NC}"
-echo -e "${CYAN}********************************************${NC}"
-echo
+echo -e "${YELLOW}Créditos del Desarrollo del Software"
 
 # Desarrolladores
-echo -e "${GREEN}Desarrolladores:${NC}"
-echo -e "${WHITE}- Nombre del Desarrollador 1${NC}"
-echo -e "${WHITE}- Nombre del Desarrollador 2${NC}"
-echo -e "${WHITE}- Nombre del Desarrollador 3${NC}"
-echo
+echo "Desarrolladores:"
+echo "Andrés Ruslan Abadías Otal"
 
 # Software usado
-echo -e "${GREEN}Software usado:${NC}"
-echo -e "${WHITE}- Software 1${NC}"
-echo -e "${WHITE}- Software 2${NC}"
-echo -e "${WHITE}- Software 3${NC}"
-echo
+echo "Traducción: DeepL"
+echo "   https://www.deepl.com/es/translator"
+echo "Revisión de Código: ShellCheck"
+echo "   https://github.com/koalaman/shellcheck"
 
 # Agradecimientos especiales
-echo -e "${GREEN}Agradecimientos especiales:${NC}"
-echo -e "${WHITE}- Persona o entidad 1${NC}"
-echo -e "${WHITE}- Persona o entidad 2${NC}"
-echo
+echo -e "Agradecimientos especiales:"
+echo -e "- Diego García"
 
 # Mensaje final
-echo -e "${BLUE}Gracias por usar nuestro software!${NC}"
-echo -e "${BLUE}---------------------------------${NC}"
+echo "Gracias por usar AutoNetplan!"
+echo "-----------------------------"
